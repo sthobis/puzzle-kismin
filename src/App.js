24d355be53fc8fa9f4bbeb1d332c1803dc527e2c
sthobis <s.thobi.sinaga@gmail.com>
2018-07-06 18:28:41 +0700

    feat: add image picker

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffleArray(array) {
-  let shuffled = array.slice()
+  let shuffled = array.slice();
   for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // eslint-disable-line no-param-reassign
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
-  return shuffled
+  return shuffled;
 }
+
+const images = [
+  "https://pamcarter.co.uk/files/cache/88090ad09668e0147f50a9da6178c2d7_f389.jpg",
+  "https://img.buzzfeed.com/buzzfeed-static/static/2018-01/23/15/asset/buzzfeed-prod-fastlane-01/anigif_sub-buzz-27474-1516737829-1.gif?downsize=715:*&output-format=auto&output-quality=auto"
+]
 
 const CONSTANT = {
   PIECES: "PIECES",
@@ -18,45 +23,37 @@ const CONSTANT = {
 
 class App extends Component {
   state = {
-    imageSrc: "https://pamcarter.co.uk/files/cache/88090ad09668e0147f50a9da6178c2d7_f389.jpg",
+    imageSrc: null,
     rows: 3,
     columns: 4,
     pieces: [],
     solution: [],
     correctAnswer: "antikismin",
-    puzzleWidth: 0
+    puzzleWidth: 0,
+    inputText: ""
   }
 
   componentDidMount() {
-    const { imageSrc } = this.state
+    this.canvasImage = null
+    this.reset(images[0])
+  }
+
+  selectImageSrc = src => e => {
+    this.reset(src)
+  }
+
+  reset = imageSrc => {
     this.setState({
       puzzleWidth: this.puzzleContainer.getBoundingClientRect().width
     }, () => {
-      this.canvasAssets = []
-      this.preloadImages(
-        [imageSrc],
-        this.canvasAssets,
-        this.createPuzzle
-      )
-    })
-  }
-
-  preloadImages(srcs, imgs, callback) {
-    let img
-    let remaining = srcs.length
-    const removeFinished = () => {
-      remaining--
-      if (remaining <= 0) {
-        callback()
+      let img = new Image()
+      img.onload = () => {
+        this.setState({ imageSrc })
+        this.createPuzzle()
       }
-    };
-
-    for (let i = 0; i < srcs.length; i++) {
-      img = new Image()
-      img.onload = removeFinished
-      img.src = srcs[i]
-      imgs.push(img)
-    }
+      img.src = imageSrc
+      this.canvasImage = img
+    })
   }
 
   createPuzzle = () => {
@@ -76,7 +73,7 @@ class App extends Component {
     canvas.height = puzzleWidth / rows
 
     const ctx = canvas.getContext("2d")
-    const background = this.canvasAssets[0]
+    const background = this.canvasImage
     const dx = -canvas.width * (index % columns)
     const dy = -canvas.height * (Math.floor(index / columns))
     ctx.drawImage(background, dx, dy, puzzleWidth, puzzleWidth)
@@ -148,7 +145,16 @@ class App extends Component {
   }
 
   render() {
-    const { imageSrc, rows, columns, pieces, solution, correctAnswer, puzzleWidth } = this.state
+    const {
+      imageSrc,
+      rows,
+      columns,
+      pieces,
+      solution,
+      correctAnswer,
+      puzzleWidth,
+      inputText
+    } = this.state
     const solved = solution.toString() === correctAnswer
 
     return (
@@ -166,10 +172,28 @@ class App extends Component {
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            background: "gold"
+            background: "gold",
+            padding: 20
           }}
         >
-          <div style={{display: "flex", alignItems: "center"}}>
+          <div>
+            <input
+              type="text"
+              value={inputText}
+              onChange={(e) => {
+                this.setState({ inputText: e.target.value })
+              }}
+              placeholder="Put image url here and press change image.."
+            />
+            <button type="button" onClick={() => { this.reset(inputText)}}>Change Image</button>
+          </div>
+          <p style={{ fontSize: 13 }}>Or just click on images below.. (gif is fun)</p>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center"
+            }}
+          >
             <div
               style={{
                 width: 200,
@@ -180,13 +204,35 @@ class App extends Component {
               }}
             >
               <img
-                ref={el => {this.puzzleThumbnail = el}}
-                src={imageSrc}
+                src={images[0]}
+                onClick={this.selectImageSrc(images[0])}
                 alt="puzzle source"
                 style={{
                   width: "100%",
                   height: "100%",
-                  objectFit: "contain"
+                  objectFit: "contain",
+                  cursor: "pointer"
+                }}
+              />
+            </div>
+            <div
+              style={{
+                width: 200,
+                height: 200,
+                marginLeft: 30,
+                marginRight: 30,
+                marginBottom: 30
+              }}
+            >
+              <img
+                src={images[1]}
+                onClick={this.selectImageSrc(images[1])}
+                alt="puzzle source"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "contain",
+                  cursor: "pointer"
                 }}
               />
             </div>
@@ -238,7 +284,8 @@ class App extends Component {
                           width: "100%",
                           height: "100%",
                           position: "relative",
-                          overflow: "hidden"
+                          overflow: "hidden",
+                          cursor: "move"
                         }}
                       >
                         <img
@@ -266,10 +313,29 @@ class App extends Component {
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            background: "aliceblue"
+            background: "aliceblue",
+            padding: 20
           }}
         >
-          <p>{solved ? "SOLVED!" : "UNSOLVED"}</p>
+          <div
+            style={{
+              marginBottom: 30
+            }}
+          >
+            <img
+              src={imageSrc}
+              alt="puzzle guide"
+              style={{
+                maxWidth: 300,
+                maxHeight: 300
+              }}
+            />
+          </div>
+          <p
+            style={{
+              color: solved ? "#B4D455" : "red"
+            }}
+          >{solved ? "SOLVED!" : "UNSOLVED"}</p>
           <div
             style={{
               width: "50%",
@@ -310,7 +376,8 @@ class App extends Component {
                           width: "100%",
                           height: "100%",
                           position: "relative",
-                          overflow: "hidden"
+                          overflow: "hidden",
+                          cursor: "move"
                         }}
                       >
                         <img
@@ -333,8 +400,8 @@ class App extends Component {
           </div>
         </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
